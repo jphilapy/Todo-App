1842ce27628c319bd93899e96cfa373c26e9203e
jphilapy <jeff@altahost.com>
2021-01-05 17:03:15 -0500

    misc

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,49 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Todo') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>{{ config('app.name', 'Todo') }}</title>
 
-        <!-- Scripts -->
-            <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
 
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    </head>
-    <body>
-    @if(Session::has('success'))
-        {{ Session::get('success') }}
-    @endif
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+</head>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+<body>
+    <div class="container">
+        <div class="card text-success text-center p-3">
+            @if(Session::has('success'))
+            {{ Session::get('success') }}
+            @else
+            Add a task below.
             @endif
+        </div>
+    </div>
 
-            <div class="content">
-                <div class="m-b-md">       
-                    @yield('content')
-                </div>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
+            @endif
+            @endauth
+        </div>
+        @endif
+
+        <div class="content">
+            <div class="m-b-md container">
+                @yield('content')
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -2,24 +2,34 @@
 @section('content')
 
 <div class="row">
-    <div class="col-lg-6 col-lg-offset-3">
-    <form action="{{ route('todo.store') }}" method="post">
-    @csrf
-        <input type="text" class="form-control input-lg" name="todo" placeholder="Create a new todo">
-    </form>
+    <div class="col-lg-12 col-lg-offset-3">
+        <form action="{{ route('todo.store') }}" method="post">
+            @csrf
+            <input type="text" class="form-control input-lg" name="todo"
+                placeholder="Type a new task and press the enter button.">
+        </form>
     </div>
 </div>
 <hr>
+<div>
     @foreach($todos as $todo)
-        <a href="{{ route('todo.delete', ['id' => $todo->id]) }}" class="btn btn-danger">X</a>
-        <a href="{{ route('todo.update', ['id' => $todo->id]) }}" class="btn btn-info btn-xs">update</a>
-        | {{ $todo->todo }} | 
-        @if(!$todo->completed)
-        <a href="{{ route('todo.complete', ['id' => $todo->id]) }}" class="btn btn-success btn-xs">complete</a>
-        @else
-        <span class="text-success">Completed!</span>
-        @endif
-       
+    <div class="row  border-bottom mb-1 pb-1 ml-2">
+        <div class="col-4 pl-0">
+            <a href="{{ route('todo.delete', ['id' => $todo->id]) }}" class="btn btn-danger w-10">X</a>
+            <a href="{{ route('todo.update', ['id' => $todo->id]) }}" class="btn btn-info btn-xs">update</a>
+        </div>
+        <div class="col-2 pt-2"> {{ $todo->todo }} </div>
+
+        <div class="col-2 pt-2">
+            @if(!$todo->completed)
+            <a href="{{ route('todo.complete', ['id' => $todo->id]) }}" class="btn btn-success btn-xs">complete</a>
+            @else
+            <span class="text-success">Completed!</span>
+            @endif
+        </div>
+
         <hr>
+    </div>
     @endforeach
+</div>
 @endsection
